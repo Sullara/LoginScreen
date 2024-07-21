126ae7e279f21b4d3ac2f0e85ef00837b878334d
Sullara <datausquelara@hotmail.com>
2024-07-21 09:04:35 -0300

    sky background video

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const mode = document.getElementById('mode-icon');
+const video = document.getElementById('lightmode-video');
 
 mode.addEventListener('click', () => {
     const form = document.getElementById('login-form');
@@ -7,11 +8,15 @@ mode.addEventListener('click', () => {
         mode.classList.remove('fa-moon');
         mode.classList.add('fa-sun');
         form.classList.add('dark');
-        
+        video.removeAttribute('./assets/Light mode clouds.mp4');
+        video.setAttribute('src', './assets/dark mode clouds.mp4');
         return;
     }
 
     mode.classList.add('fa-moon');
     mode.classList.remove('fa-sun');
     form.classList.remove('dark');
+    video.removeAttribute('./assets/dark mode clouds.mp4');
+    video.setAttribute('src', './assets/Light mode clouds.mp4');
+    
 });
